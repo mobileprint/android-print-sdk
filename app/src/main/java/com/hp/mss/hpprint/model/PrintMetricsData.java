bdeb507d1fdfbc83650c7d47bb45189709143f37
Li <li.feng@hp.com>
2015-05-14 15:14:53 -0700

    US3383 - refactor/implement metrics

---
+++ app/src/main/java/com/hp/mss/hpprint/model/PrintMetricsData.java
@@ -0,0 +1,114 @@
+package com.hp.mss.hpprint.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.print.PrintJob;
+import android.util.Log;
+
+import com.hp.mss.hpprint.util.PrintUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by panini on 5/13/15.
+ */
+public class PrintMetricsData implements Parcelable {
+
+
+    public static final String DATA_NOT_AVAILABLE = "No data available";
+    public static final String CONTENT_TYPE_DOCUMENT = "Document";
+    public static final String CONTENT_TYPE_PHOTO = "Photo";
+    public static final String CONTENT_TYPE_UNKNOWN = "Unknown";
+
+    PrintJob printJob;
+    PrintUtil.OnPrintDataCollectedListener collectedListener;
+
+    public String blackAndWhiteFilter;
+    public String numberOfCopy;
+    public String paperSize;
+    public String paperType;
+    public String printPluginTech;
+    public String printerID;
+    public String printerLocation;
+    public String printerModel;
+    public String printerName;
+
+    public PrintMetricsData() {
+//        this.printerID = DATA_NOT_AVAILABLE;
+//        this.printerLocation = DATA_NOT_AVAILABLE;
+//        this.printerModel = DATA_NOT_AVAILABLE;
+//        this.printerName = DATA_NOT_AVAILABLE;
+    }
+
+    public PrintMetricsData(Parcel in) {
+        blackAndWhiteFilter = in.readString();
+        numberOfCopy = in.readString();
+        paperSize = in.readString();
+        paperType = in.readString();
+        printPluginTech = in.readString();
+        printerID = in.readString();
+        printerLocation = in.readString();
+        printerModel = in.readString();
+        printerName = in.readString();
+    }
+
+    public PrintMetricsData(Map<String, String> map) {
+        this.blackAndWhiteFilter = map.get("black_and_white_filter");
+        this.numberOfCopy = map.get("copies");
+        this.paperSize = map.get("paper_size");
+        this.paperType = map.get("paper_type");
+        this.printPluginTech = map.get("print_plugin_tech");
+        this.printerID = map.get("printer_id");
+        this.printerLocation = map.get("printer_location");
+        this.printerModel = map.get("printer_model");
+        this.printerName = map.get("printer_name");
+    }
+
+    public Map<String,String> toMap() {
+        Map<String, String> map = new HashMap<String, String>();
+
+        map.put("black_and_white_filter", this.blackAndWhiteFilter);
+        map.put("copies", this.numberOfCopy);
+        map.put("paper_size", this.paperSize);
+        map.put("paper_type", this.paperType);
+        map.put("print_plugin_tech", this.printPluginTech);
+        map.put("printer_id", this.printerID);
+        map.put("printer_location", this.printerLocation);
+        map.put("printer_model", this.printerModel);
+        map.put("printer_name", this.printerName);
+
+        return map;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(blackAndWhiteFilter);
+        out.writeString(numberOfCopy);
+        out.writeString(paperSize);
+        out.writeString(paperType);
+        out.writeString(printPluginTech);
+        out.writeString(printerID);
+        out.writeString(printerLocation);
+        out.writeString(printerModel);
+        out.writeString(printerName);
+
+   }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public PrintMetricsData createFromParcel(Parcel in) {
+            return new PrintMetricsData(in);
+        }
+
+        public PrintMetricsData[] newArray(int size) {
+            return new PrintMetricsData[size];
+        }
+    };
+
+}
+
---
+++ app/src/main/java/com/hp/mss/hpprint/util/PrintMetricsCollector.java
@@ -0,0 +1,107 @@
+package com.hp.mss.hpprint.util;
+
+import android.content.ComponentName;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentInfo;
+import android.print.PrintJob;
+import android.print.PrintJobInfo;
+import android.print.PrinterId;
+import android.util.Log;
+
+import com.hp.mss.hpprint.model.PrintMetricsData;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import android.os.Handler;
+
+
+/**
+ * Created by panini on 5/18/15.
+ */
+
+
+class PrintMetricsCollector extends Thread {
+    public static final int PRINT_JOB_WAIT_TIME = 1000;
+    public static final int MILS = 1000;
+
+    android.print.PrintJob printJob;
+    PrintUtil.OnPrintDataCollectedListener collectedListener;
+    Handler metricsHandler;
+
+    public PrintMetricsCollector(PrintJob printJob, PrintUtil.OnPrintDataCollectedListener collectedListener) {
+        this.printJob = printJob;
+        this.collectedListener = collectedListener;
+        this.metricsHandler = new Handler();
+    }
+
+    @Override
+    public void run() {
+
+        if ( printJob == null || collectedListener == null || isJobFailed(printJob) ) {
+
+            return;
+
+        } else if ( hasJobInfo(printJob) ) {
+
+            PrintJobInfo printJobInfo = printJob.getInfo();
+            PrintAttributes printJobAttributes = printJobInfo.getAttributes();
+            PrinterId printerId = printJobInfo.getPrinterId();
+
+            PrintMetricsData metricsData = new PrintMetricsData();
+
+            try {
+                Method gdi = PrintJobInfo.class.getMethod("getDocumentInfo");
+                PrintDocumentInfo printDocumentInfo = (PrintDocumentInfo) gdi.invoke(printJobInfo);
+                Method gsn = PrinterId.class.getMethod("getServiceName");
+                ComponentName componentName = (ComponentName) gsn.invoke(printerId);
+
+                metricsData.printPluginTech = componentName.getPackageName();
+
+                if (printDocumentInfo.getContentType() == PrintDocumentInfo.CONTENT_TYPE_DOCUMENT) {
+                    metricsData.paperType = PrintMetricsData.CONTENT_TYPE_DOCUMENT;
+                } else if (printDocumentInfo.getContentType() == PrintDocumentInfo.CONTENT_TYPE_PHOTO) {
+                    metricsData.paperType = PrintMetricsData.CONTENT_TYPE_PHOTO;
+                } else if (printDocumentInfo.getContentType() == PrintDocumentInfo.CONTENT_TYPE_UNKNOWN) {
+                    metricsData.paperType = PrintMetricsData.CONTENT_TYPE_UNKNOWN;
+                }
+
+                metricsData.blackAndWhiteFilter = String.valueOf(printJobInfo.getAttributes().getColorMode());
+
+                String width = Double.toString(printJobAttributes.getMediaSize().getWidthMils() / (float) MILS);
+                String height = Double.toString(printJobAttributes.getMediaSize().getHeightMils() / (float) MILS);
+
+                metricsData.paperSize =  (width + " x " + height).toString();
+                metricsData.printerID = printerId.getLocalId();
+                metricsData.numberOfCopy = String.valueOf(printJobInfo.getCopies());
+
+                collectedListener.postPrintData(metricsData);
+            } catch (NoSuchMethodException e) {
+                Log.e("ERROR", "CollectionRunner: " + e.getMessage());
+            } catch (InvocationTargetException e) {
+                Log.e("ERROR", "CollectionRunner: " + e.getMessage());
+            } catch (IllegalAccessException e) {
+                Log.e("ERROR", "CollectionRunner: " + e.getMessage());
+            }
+
+        } else {
+
+            metricsHandler.postDelayed(this, PRINT_JOB_WAIT_TIME);
+
+        }
+    }
+
+
+    private static boolean isJobDone(final android.print.PrintJob printJob) {
+        return   (printJob.isQueued() || printJob.isCompleted() || printJob.isStarted() );
+    }
+
+    private static boolean hasJobInfo(final android.print.PrintJob printJob) {
+        return ( printJob.isQueued() || printJob.isCompleted() || printJob.isStarted() );
+    }
+
+    private static boolean isJobFailed(final android.print.PrintJob printJob) {
+        return ( printJob.isFailed() || printJob.isBlocked() || printJob.isCancelled() );
+    }
+}
+
+
