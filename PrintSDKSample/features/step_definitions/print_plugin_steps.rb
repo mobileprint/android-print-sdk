f1ce2bb32a5178daba43b37e68d9d65f38403617
Needu <needu.damodaran@accelfrontline.com>
2016-02-18 16:24:39 +0530

    Fix in print metrics and print features

--- PrintSDKSample/features/step_definitions/print_plugin_steps.rb
+++ PrintSDKSample/features/step_definitions/print_plugin_steps.rb
@@ -111,8 +111,8 @@ Then (/^I select Quality as "([^"]*)"$/) do |quality|
     
     sleep(APPIUM_TIMEOUT)
     $Quality = quality
-    wait.until { selenium.find_element(:xpath,"//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.Spinner[1]/android.widget.TextView[1]") }
-    selenium.find_element(:xpath,"//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.Spinner[1]/android.widget.TextView[1]").click
+    wait.until { selenium.find_element(:xpath,"//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.Spinner[1]/android.widget.TextView[1]") }
+    selenium.find_element(:xpath,"//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.Spinner[1]/android.widget.TextView[1]").click
     
     wait.until {selenium.find_elements(:xpath,"//android.widget.ListView[1]/android.widget.CheckedTextView[@text='#{quality}']")}
         if selenium.find_elements(:xpath,"//android.widget.CheckedTextView[@text='#{quality}']").size > 0
